bf4a346c273153459c6a348a02f3cfa50d2c3e50
kaioum675 <dzkimo03@gmail.com>
2026-01-23 16:47:01 +0100

    Update s2.js

--- s2.js
+++ s2.js
@@ -8,35 +8,28 @@ if (tt> 10) {
   
 }
 function calcu() {
-  var probtd = Number(document.getElementById('probtd').value) ;
+  var probtd = Number(document.getElementById('hydtd').value) ;
 
  
 
-  var probctrl = Number(document.getElementById('probctrl').value);
+  var probctrl = Number(document.getElementById('hydctrl').value);
 
-   var probtt = document.getElementById('probtt').value =  Number((probtd)*0.33+(probctrl*0.67));
-  var algotd = Number(document.getElementById('algotd').value) ;
-
- 
-
-  var algoctrl = Number(document.getElementById('algoctrl').value);
-
-  var algotp = Number(document.getElementById('algotp').value);
+   var probtt = document.getElementById('hydtt').value =  Number((hydtd)*0.4+(hydctrl*0.6));
+  var algotd = Number(document.getElementById('batd').value) ;
 
  
 
-  var algott = document.getElementById('algott').value =  Number((algotd)*0.16+(algoctrl*0.68)+(algotp)*0.16);
+  var algoctrl = Number(document.getElementById('bactrl').value);
+ 
+  var algott = document.getElementById('batt').value =  Number((batd)*0.4+(bactrl*0.6));
 
-
-
-
-    var alg1ntd = Number(document.getElementById('alg1ntd').value) ;
+    var alg1ntd = Number(document.getElementById('rdmtd').value) ;
 
    
 
-    var alg1ctrl = Number(document.getElementById('alg1ctrl').value);
+    var alg1ctrl = Number(document.getElementById('rdmctrl').value);
 
-    var alg1tt = document.getElementById('alg1tt').value = Number((((alg1ntd)*0.33)+(alg1ctrl*0.67)));
+    var alg1tt = document.getElementById('rdmtt').value = Number((((rdmtd)*0.4)+(rdmctrl*0.6)));
 
 
  
@@ -44,73 +37,39 @@ function calcu() {
 
 
 
-    var ana1ntd = Number(document.getElementById('ana1ntd').value) ;
+    var ana1ntd = Number(document.getElementById('mdstd').value) ;
 
    
 
-    var ana1ctrl = Number(document.getElementById('ana1ctrl').value);
+    var ana1ctrl = Number(document.getElementById('mdsctrl').value);
+    var ana1ntd = Number(document.getElementById('mdstp').value) ;
 
-    var ana1tt = document.getElementById('ana1tt').value = Number((((ana1ntd)*0.34)+(ana1ctrl*0.66)));
-  
-
-var fund=document.getElementById("fund").innerHTML = ((probtt)+(2*alg1tt)+(2*ana1tt))/5;
-
-  var fundcr= document.getElementById('fundcr').innerHTML = crid(fund,11,0);
+    var ana1tt = document.getElementById('mdstt').value = Number((mdstd*0.2)+(mdsctrl*0.6)+(mdstp*0.2));
 
 
-  var tictt = Number(document.getElementById('tictt').value);
-    var ipotp = Number(document.getElementById('ipotp').value) ;
+  var tictt = Number(document.getElementById('topotp').value);
+    var ipotp = Number(document.getElementById('topoctrl').value) ;
 
-   
+    var ipott = document.getElementById('topott').value = Number((((topotp)*0.4)+(topoctrl*0.6)));
 
-    var ipoctrl = Number(document.getElementById('ipoctrl').value);
+    var electd = Number(document.getElementById('pgctt').value) ;
 
-    var ipott = document.getElementById('ipott').value = Number((((ipotp)*0.4)+(ipoctrl*0.6)));
 
-      var termtt = Number(document.getElementById('termtt').value);
+
+
+    var electrl = Number(document.getElementById('dessintt').value);
 
   
-
-var metodo =document.getElementById("metodo").innerHTML = ((tictt*2)+(ipott))/3;
-
-  var metodocr = document.getElementById('metodocr').innerHTML = crid(metodo,7,0);
-
-
-
-
-
-
-
-
-    
+var codtd = Number(document.getElementById('anglaistt').value) ;
 
   
 
-    
+    var mdctp = Number(document.getElementById('mdctp').value);
+    var mdctrl = Number(document.getElementById('mdctrl').value);
+    var mdctt = document.getElementById('mdctt').value = Number((((mdctp)*0.4)+(mdctrl*0.6)));
 
-    var electd = Number(document.getElementById('electd').value) ;
+    var geott = Number(document.getElementById('geott').value);
 
-
-
-
-    var electrl = Number(document.getElementById('electrl').value);
-
-    var electt = document.getElementById('electt').value = Number((((electd)*0.3)+(electrl*0.7)));
-
-
-var codtd = Number(document.getElementById('codtd').value) ;
-
-  
-
-    var codctrl = Number(document.getElementById('codctrl').value);
-
-    var codtt = document.getElementById('codtt').value = Number((((codtd)*0.34)+(codctrl*0.66)));
-
-
-
-  
-
-var tran= document.getElementById("tran").innerHTML = Number(((termtt)+(electt*2))/3);
 
   var trancr= document.getElementById('trancr').innerHTML = crid(tran,3,0);
 var deco= document.getElementById("deco").innerHTML = ((codtt*2)+(algott*3))/5;
